f221b65461d8a873db2f460b671cdbd2802fbeba
scit106 <scit106@gmail.com>
2013-08-18 19:44:08 -0400

    able to create new companies now

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -9,21 +9,25 @@ class CompaniesController < ApplicationController
 	end
 
 	def create
-		@company = Company.new
-		@company.save
+		@company = Company.new(params[:company])
+		if @company.save
+			redirect_to companies_path
+		else
+			redirect_to new_company_path
+		end
 	end
 
 	def edit
-		@company = Company.find[params(:id)]
+		@company = Company.find(params[:id])
 	end
 
 	def update
-		@company = Company.find[params(:id)]
+		@company = Company.find(params[:id])
 		@company.update_attributes
 	end
 
 	def destroy
-		@company = Company.find[params(:id)]
+		@company = Company.find(params[:id])
 		@company.destroy
 	end
 
